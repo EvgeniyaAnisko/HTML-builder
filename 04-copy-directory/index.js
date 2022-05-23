7be6b43e5348303ef17e28bfc50740868c80153e
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-05-23 20:47:36 +0300

    refactor: removed additional code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,7 @@
 const path = require('path');
-const { copyFile, mkdir, readdir, unlink } = require('fs/promises');
+const { copyFile, mkdir, readdir } = require('fs/promises');
 
 mkdir(path.join(__dirname, 'copy'), {recursive: true});
-
-readdir(path.join(__dirname, 'copy'), {withFileTypes: true}).then(res => {
-  for (let item of res)
-    unlink(path.join(__dirname, 'copy', item.name));
-});
 
 readdir(path.join(__dirname, 'files'), {withFileTypes: true}).then(res => {
   for (let item of res)
