926bf2ce6c9fcf73f4a2d1f5a98268c389470687
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-05-24 17:01:45 +0300

    fix: remove files in copy directory before copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,12 @@
 const path = require('path');
-const { copyFile, mkdir, readdir } = require('fs/promises');
+const { copyFile, mkdir, readdir, unlink } = require('fs/promises');
 
 mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
+
+readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true}).then(res => {
+  for (let item of res)
+    unlink(path.join(__dirname, 'files-copy', item.name));
+});
 
 readdir(path.join(__dirname, 'files'), {withFileTypes: true}).then(res => {
   for (let item of res)
