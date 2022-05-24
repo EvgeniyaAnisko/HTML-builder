81f8a575f869b39422dfce9063fdd164b066f0c5
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-05-24 16:28:24 +0300

    fix: rename folder copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const { copyFile, mkdir, readdir } = require('fs/promises');
 
-mkdir(path.join(__dirname, 'copy'), {recursive: true});
+mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
 
 readdir(path.join(__dirname, 'files'), {withFileTypes: true}).then(res => {
   for (let item of res)
-    copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'copy', item.name));  
+    copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name));  
 });
